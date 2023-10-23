6d35ac77d7628e442fd8136b28d074aac05b4e7f
elvinzulfuqarli <elvinzulfuqarli714@gmail.com>
2023-10-23 13:37:34 +0400

    added TheHero component

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,20 +1,71 @@
 <template>
+  <div class="bg-[#6075AD] border-b border-[#DD5231] relative h-[238px]">
+    <img
+      class="py-[6px]"
+      width="228.58"
+      height="85.3"
+      src="../static/a1.png"
+      alt=""
+    />
+    <!-- <div class="absolute top-0 right-0">
+      <img width="16" height="12" src="../static/V1.png" alt="" />
+      <img width="32" height="24" src="../static/V2.png" alt="" />
+      <img width="48" height="36" src="../static/V3.png" alt="" />
+      <img width="60" height="40" src="../static/V4.png" alt="" />
+    </div> -->
+    <div class="pl-[180px] py-[20px]">
+      <p class="text-[48px] not-italic font-bold leading-none text-[#FFF]">
+        VETERINARY!
+      </p>
+      <br />
+      <p class="text-[24px] not-italic font-normal leading-none text-[#FFF]">
+        What does a Veterinarian Do?
+      </p>
+    </div>
+  </div>
 
+  <div class="absolute top-[233px] left-[613px] grid grid-cols-2 pr-8">
+    <div class="pr-[40px]"><img src="../static/dogs.png" alt="" /></div>
+    <div class="pr-[20px]"><img src="../static/cat.png" alt="" /></div>
+  </div>
+  <div class="flex gap-10">
+    <img src="../static/a3.png" alt="" />
+    <p class="w-96 mt-16">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit animi ex
+      autem ab sunt est blanditiis, eveniet cupiditate natus, asperiores maiores
+      minus exercitationem error maxime harum temporibus reprehenderit?
+      Mollitia, maxime.
+    </p>
+  </div>
+  <div class="text-[48px] text-[#3D3689] pl-[150px] ">OUR VETS</div>
 
-<div class="w-[1440px] h-[252px] bg-[#6075AD] border-b border-[#DD5231]">
-
- <div> <p class="text-[48px] not-italic font-bold leading-none text-[#FFF] ">VETERINARY!</p>
-  <p class="text-[24px] not-italic font-normal leading-none text-[#FFF]">What does a Veterinarian Do?</p>
- </div>
-</div>
-
-<div>
-  <img src="../static/dogs.png" alt="">
-  <img src="../static/cat.png" alt="">
-  <img src="" alt="">
-  <img src="" alt="">
-</div>
-
-
-
+  <div class="flex gap-10 pl-[160px] ">
+    <div class="border border-gray-200 pl-[77px]" >
+      <img src="../static/doktor.png" alt="" />
+      <p class="text-[24px] text-[#000] font-medium">Name Surname</p><br>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+        volutpat. Ut wisi enim
+      </p>
+    </div>
+    <div class="border border-gray-200 pl-[77px]">
+      <img src="../static/doktor1.png" alt="" />
+      <p class="text-[24px] text-[#000] font-medium">Name Surname</p><br>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+        volutpat. Ut wisi enim
+      </p>
+    </div>
+    <div class="border border-gray-200 pl-[77px]">
+      <img src="../static/doktor2.png" alt="" />
+      <p class="text-[24px] text-[#000] font-medium">Name Surname</p><br>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+        volutpat. Ut wisi enim
+      </p>
+    </div>
+  </div>
 </template>
